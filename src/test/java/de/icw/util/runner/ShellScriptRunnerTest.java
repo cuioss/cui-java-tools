b7d86e8bac610e60802a04cbb3fffb1d36d5d550
Oliver Wolff <oliver.wolff@icw.de>
2019-04-12 17:09:08 +0200

    Update ShellScriptRunnerTest.java

--- src/test/java/de/icw/util/runner/ShellScriptRunnerTest.java
+++ src/test/java/de/icw/util/runner/ShellScriptRunnerTest.java
@@ -4,13 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class ShellScriptRunnerTest {
 
     @Test
-    @Disabled
     void shouldRunOsSpecificRunScript() {
         ShellScriptRunner runner = new ShellScriptRunner(ScriptMetadataTest.SCRIPT_METADATA);
         assertEquals(AbstractApplicationRunner.State.NEW, runner.getState());
