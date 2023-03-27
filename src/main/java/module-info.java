889b0d9ac3cff7416c7f3a4535c3d7c57107abb6
Sven Haag <sven.haag@x-tention.de>
2023-03-27 15:26:16 +0200

    Add module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,21 @@
+module de.cuioss.java.tools {
+    requires lombok;
+    requires java.desktop;
+    requires java.logging;
+
+    exports de.cuioss.tools.base;
+    exports de.cuioss.tools.codec;
+    exports de.cuioss.tools.collect;
+    exports de.cuioss.tools.concurrent;
+    exports de.cuioss.tools.formatting;
+    exports de.cuioss.tools.formatting.template;
+    exports de.cuioss.tools.formatting.template.lexer;
+    exports de.cuioss.tools.formatting.template.token;
+    exports de.cuioss.tools.io;
+    exports de.cuioss.tools.lang;
+    exports de.cuioss.tools.net;
+    exports de.cuioss.tools.net.ssl;
+    exports de.cuioss.tools.property;
+    exports de.cuioss.tools.reflect;
+    exports de.cuioss.tools.string;
+}
