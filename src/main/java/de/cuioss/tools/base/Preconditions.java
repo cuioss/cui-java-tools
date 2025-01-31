731a8bee160b0e4c4d3415104c6b20595e5f2b2f
Oliver Wolff <23139298+cuioss@users.noreply.github.com>
2025-01-31 12:08:32 +0100

    refactor: Improve code quality in base package
    - Enhance JavaDoc formatting and clarity in BooleanOperations and Preconditions
    - Fix grammar and consistency in method documentation
    - Improve code organization and readability
    - Fix import ordering in Preconditions.java
    - Remove redundant null check in isValidBoolean

--- src/main/java/de/cuioss/tools/base/Preconditions.java
+++ src/main/java/de/cuioss/tools/base/Preconditions.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2023 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * https://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,9 +15,9 @@
  */
 package de.cuioss.tools.base;
 
-import static de.cuioss.tools.string.MoreStrings.lenientFormat;
-
 import lombok.experimental.UtilityClass;
+
+import static de.cuioss.tools.string.MoreStrings.lenientFormat;
 
 /**
  * Static utility methods for validating method arguments and state. This class
@@ -137,14 +137,13 @@ import lombok.experimental.UtilityClass;
 public class Preconditions {
 
     /**
-     * 
-     * Inspired by Google Guava Preconditions.
-     *
-     * @param expression a boolean expression
-     * @throws IllegalArgumentException if {@code expression} is false
-     * @see <a href="https://github.com/google/guava/blob/master/guava/src/com/google/common/base/Preconditions.java">Google Guava</a>
      * Ensures the truth of an expression involving one or more parameters to the
      * calling method.
+     * Inspired by Google Guava Preconditions.
+     *
+     * @param expression a boolean expression that should evaluate to true for valid arguments
+     * @throws IllegalArgumentException if {@code expression} is false
+     * @see <a href="https://github.com/google/guava/blob/master/guava/src/com/google/common/base/Preconditions.java">Google Guava</a>
      */
     public static void checkArgument(boolean expression) {
         if (!expression) {
@@ -153,15 +152,14 @@ public class Preconditions {
     }
 
     /**
-     * 
-     * Inspired by Google Guava Preconditions.
-     *
-     * @param expression a boolean expression
-     * @param message    to be put into the create {@link IllegalArgumentException}
-     * @throws IllegalArgumentException if {@code expression} is false
-     * @see <a href="https://github.com/google/guava/blob/master/guava/src/com/google/common/base/Preconditions.java">Google Guava</a>
      * Ensures the truth of an expression involving one or more parameters to the
      * calling method.
+     * Inspired by Google Guava Preconditions.
+     *
+     * @param expression a boolean expression that should evaluate to true for valid arguments
+     * @param message    to be put into the created {@link IllegalArgumentException}
+     * @throws IllegalArgumentException if {@code expression} is false
+     * @see <a href="https://github.com/google/guava/blob/master/guava/src/com/google/common/base/Preconditions.java">Google Guava</a>
      */
     public static void checkArgument(boolean expression, String message) {
         if (!expression) {
@@ -170,25 +168,27 @@ public class Preconditions {
     }
 
     /**
-     * 
-     * Inspired by Google Guava Preconditions.
+     * Ensures the truth of an expression involving one or more parameters to the
+     * calling method. Inspired by Google Guava Preconditions.
      *
-     * @param expression           a boolean expression
+     * @param expression           a boolean expression that should evaluate to true for valid arguments
      * @param errorMessageTemplate a template for the exception message should the
-     *                             check fail. The message is formed by replacing
+     *                             check fail.
+     *                             The message is formed by replacing
      *                             each {@code %s} placeholder in the template with
-     *                             an argument. These are matched by position - the
+     *                             an argument.
+     *                             These are matched by position - the
      *                             first {@code %s} gets {@code
-     *                             errorMessageArgs[0]} , etc. Unmatched arguments will be appended to
-     *                             the formatted message in square braces. Unmatched
+     *                             errorMessageArgs[0]}, etc. Unmatched arguments will be appended to
+     *                             the formatted message in square braces.
+     *                             Unmatched
      *                             placeholders will be left as-is.
      * @param errorMessageArgs     the arguments to be substituted into the message
-     *                             template. Arguments are converted to strings
+     *                             template.
+     *                             Arguments are converted to strings
      *                             using {@link String#valueOf(Object)}.
      * @throws IllegalArgumentException if {@code expression} is false
      * @see <a href="https://github.com/google/guava/blob/master/guava/src/com/google/common/base/Preconditions.java">Google Guava</a>
-     * Ensures the truth of an expression involving one or more parameters to the
-     * calling method.
      */
     public static void checkArgument(boolean expression, String errorMessageTemplate, Object... errorMessageArgs) {
         if (!expression) {
@@ -197,14 +197,13 @@ public class Preconditions {
     }
 
     /**
-     * 
-     * Inspired by Google Guava Preconditions.
-     *
-     * @param expression a boolean expression
-     * @throws IllegalStateException if {@code expression} is false
-     * @see <a href="https://github.com/google/guava/blob/master/guava/src/com/google/common/base/Preconditions.java">Google Guava</a>
      * Ensures the truth of an expression involving the state of the calling
      * instance, but not involving any parameters to the calling method.
+     * Inspired by Google Guava Preconditions.
+     *
+     * @param expression a boolean expression that should evaluate to true for valid state
+     * @throws IllegalStateException if {@code expression} is false
+     * @see <a href="https://github.com/google/guava/blob/master/guava/src/com/google/common/base/Preconditions.java">Google Guava</a>
      */
     public static void checkState(boolean expression) {
         if (!expression) {
@@ -213,15 +212,14 @@ public class Preconditions {
     }
 
     /**
-     * 
-     * Inspired by Google Guava Preconditions.
-     *
-     * @param expression a boolean expression
-     * @param message    to be put into the create {@link IllegalStateException}
-     * @throws IllegalStateException if {@code expression} is false
-     * @see <a href="https://github.com/google/guava/blob/master/guava/src/com/google/common/base/Preconditions.java">Google Guava</a>
      * Ensures the truth of an expression involving the state of the calling
      * instance, but not involving any parameters to the calling method.
+     * Inspired by Google Guava Preconditions.
+     *
+     * @param expression a boolean expression that should evaluate to true for valid state
+     * @param message    to be put into the created {@link IllegalStateException}
+     * @throws IllegalStateException if {@code expression} is false
+     * @see <a href="https://github.com/google/guava/blob/master/guava/src/com/google/common/base/Preconditions.java">Google Guava</a>
      */
     public static void checkState(boolean expression, String message) {
         if (!expression) {
@@ -230,25 +228,28 @@ public class Preconditions {
     }
 
     /**
-     * 
+     * Ensures the truth of an expression involving the state of the calling
+     * instance, but not involving any parameters to the calling method.
      * Inspired by Google Guava Preconditions.
      *
-     * @param expression           a boolean expression
+     * @param expression           a boolean expression that should evaluate to true for valid state
      * @param errorMessageTemplate a template for the exception message should the
-     *                             check fail. The message is formed by replacing
+     *                             check fail.
+     *                             The message is formed by replacing
      *                             each {@code %s} placeholder in the template with
-     *                             an argument. These are matched by position - the
+     *                             an argument.
+     *                             These are matched by position - the
      *                             first {@code %s} gets {@code
-     *                             errorMessageArgs[0]} , etc. Unmatched arguments will be appended to
-     *                             the formatted message in square braces. Unmatched
+     *                             errorMessageArgs[0]}, etc. Unmatched arguments will be appended to
+     *                             the formatted message in square braces.
+     *                             Unmatched
      *                             placeholders will be left as-is.
      * @param errorMessageArgs     the arguments to be substituted into the message
-     *                             template. Arguments are converted to strings
+     *                             template.
+     *                             Arguments are converted to strings
      *                             using {@link String#valueOf(Object)}.
      * @throws IllegalStateException if {@code expression} is false
      * @see <a href="https://github.com/google/guava/blob/master/guava/src/com/google/common/base/Preconditions.java">Google Guava</a>
-     * Ensures the truth of an expression involving one or more parameters to the
-     * calling method.
      */
     public static void checkState(boolean expression, String errorMessageTemplate, Object... errorMessageArgs) {
         if (!expression) {
